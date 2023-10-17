4e5708a9523f92e5d8c71f9190d4696bc6861b07
Bugsyspot <147054145+Bugsyspot@users.noreply.github.com>
2023-10-17 11:11:37 -0700

    Add files via upload

---
+++ Prompt1.py
@@ -0,0 +1,17 @@
+#Prompt 1 prints my name and two sentences about my favorite movie and food
+
+#This defines what will be printed in the program
+def PrintPrompt1():
+	print("Jakob Swisher \nMy favorite movie is Caddyshack. \nAlso food is chicken fried steak.")
+
+#This is the main program to be performed
+def main():
+	PrintPrompt1()
+
+#This is just a failsafe to execute if something goes wrong
+if __name__=="__main__":
+	main()
+
+
+
+
---
+++ Prompt2.py
@@ -0,0 +1,13 @@
+#Prompt 2 is a program that prints the sum, difference, and product of different numbers
+
+#This defines our varibles to their numbers
+a = 5.5
+b = 7.4
+c = 10
+d = 22
+
+#This executes and prints the command
+
+print ("The sum is", a + b)
+print ("The difference is", c - d)
+print ("The product is", b * c)
